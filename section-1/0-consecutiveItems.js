60930e665ca516e9dc15b0ced0a277dddad26e6f
nc-hev-mag <150343854+nc-hev-mag@users.noreply.github.com>
2024-10-11 11:30:49 +0100

    restructure section 1

--- section-1/0-consecutiveItems.js
+++ section-1/0-consecutiveItems.js
@@ -1,7 +1,7 @@
 const { check, runTest, skipTest } = require("../test-api/index.js");
 
 function consecutiveItems(integers, a, b) {
-/*
+	/*
 You are given a list of unique integers arr, and two integers a and b. 
 Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise).
 
@@ -9,19 +9,16 @@ It is guaranteed that a and b are both present in arr.
 */
 }
 
-runTest(
-  "returns true when the given numbers are consecutive",
-  function () {
-    check(consecutiveItems([1, 2, 3, 4], 1, 2)).isEqualTo(true);
-    check(consecutiveItems([1, 2, 3, 4], 2, 3)).isEqualTo(true);
-    check(consecutiveItems([1, -12, 4, 11, , 58, -111, 3], -111, 3)).isEqualTo(true);
-  }
-);
+runTest("returns true when the given numbers are consecutive", function () {
+	check(consecutiveItems([1, 2, 3, 4], 1, 2)).isEqualTo(true);
+	// add more check() assertions here
+});
 
 skipTest(
-  "returns false when the given numbers are not consecutive",
-  function () {
-    check(consecutiveItems([1, 2, 3, 4], 1, 3)).isEqualTo(false);
-    check(consecutiveItems([1, 16, -7, 90, -8], 16, -8)).isEqualTo(false);
-  }
+	"change this to describe the behaviour you are testing in this block",
+	function () {
+		// ...add check() assertions here
+	}
 );
+
+// ...and add more test blocks down here!
