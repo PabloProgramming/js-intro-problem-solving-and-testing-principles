61f37911409256f604792318a1344aef80fed6b3
nc-hev-mag <150343854+nc-hev-mag@users.noreply.github.com>
2024-10-11 11:31:06 +0100

    add example

--- section-2/1-catAndMouse.js
+++ section-2/1-catAndMouse.js
@@ -1,35 +1,26 @@
 const { check, runTest, skipTest } = require("../test-api/index.js");
 
-// cat and mouse positions with multiple mice, how many would the cat catch
+function calculateJump(locations, jumpLength) {
+	/*
+  You will be given an array containing string representations of the locations of a cat and a mouse. The array may also contains walls represented by "W".
+  You will also be given a positive integer which represents how far the cat can jump.
 
-// cat move and mouse move, place to hide
+  Your task is to calculate if the cat can jump far enough to catch the mouse. 
 
-function calculateJump(locations, jumpLength){
-/*
-You will be given an array containing string representations of the locations of a cat and a mouse. The array may also contains walls represented by "W".
-You will also be given a positive integer which represents how far the cat can jump.
+  Each space "x" expends 1 of the cat's movement, and each wall "W" expends 2. 
+  It does not matter if the cat is before or after the mouse in the array.
 
-Your task is to calculate if the cat can jump far enough to catch the mouse. 
-
-Each space "x" expends 1 of the cat's movement, and each wall "W" expends 2. 
-It does not matter if the cat is before or after the mouse in the array.
-*/
+  E.g. 
+    calculateJump(["cat", "W", "mouse"], 5)
+    // => true
+  */
 }
 
-runTest(
-  "cat catches the mouse when it's closer than the jump length",
-  function () {
-    check(calculateJump(['cat', 'W', 'mouse'], 5)).isEqualTo(true);
-    check(calculateJump(['cat', 'x', 'x', 'mouse'], 3)).isEqualTo(true);
-    check(calculateJump(['x', 'mouse', 'W', 'x', 'cat'], 4)).isEqualTo(true);
-  }
+skipTest(
+	"change this to describe the behaviour you are testing in this block",
+	function () {
+		// ...add check() assertions here
+	}
 );
 
-skipTest(
-  "cat fails to catch the mouse when jump length is smaller than distance",
-  function () {
-    check(calculateJump(['cat', 'W', 'mouse'], 2)).isEqualTo(false);
-    check(calculateJump(['x', 'cat', 'x', 'x','mouse'], 2)).isEqualTo(false);
-    check(calculateJump(['mouse', 'x', 'W', 'x', 'W','cat'], 5)).isEqualTo(false);
-  }
-);
+// ...and add more test blocks down here!
